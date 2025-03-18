35fee21cddc148c01338e8b3af6010f363c1b7b7
Zishen Wan <zwan63@kl2125-01.ece.gatech.edu>
2025-03-18 00:06:30 -0400

    update ISPASS25 AE

--- rundir-accelergy/create_action_count.sh
+++ rundir-accelergy/create_action_count.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-python3 create_action_count.py --saved_folder /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/test_runs --run_name scale_example_run_32x32_os --arch_name systolic_array --SRAM_row_size 2 --DRAM_row_size 2 --config /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/configs/scale.cfg
+python3 create_action_count.py --saved_folder /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs --run_name scale_example_run_128x128_ws --arch_name systolic_array --SRAM_row_size 2 --DRAM_row_size 2 --config /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/configs/ispass_ae_run_128x128_ws.cfg
 
-cp /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/test_runs/scale_example_run_32x32_os/action_count.yaml ./accelergy_input/action_count.yaml
+cp /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs/scale_example_run_128x128_ws/action_count.yaml ./accelergy_input/action_count.yaml
 
-rm -rf /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/rundir-accelergy/output/scale_sim_output_scale_example_run_32x32_os
-
-mv /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/test_runs/scale_example_run_32x32_os  /home/zwan63/Research/scalesim_v3_ispass/scale-sim-v2/rundir-accelergy/output/scale_sim_output_scale_example_run_32x32_os
+mv /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs/scale_example_run_128x128_ws  /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/rundir-accelergy/output/scale_sim_output_scale_example_run_128x128_ws
 
