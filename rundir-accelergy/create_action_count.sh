6d59f0b7d809b3654ee3a4803ff1f13176ba882c
Zishen Wan <zwan63@kl2125-01.ece.gatech.edu>
2025-03-18 00:10:09 -0400

    fix ISPASS25 energy AE bug

--- rundir-accelergy/create_action_count.sh
+++ rundir-accelergy/create_action_count.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 create_action_count.py --saved_folder /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs --run_name scale_example_run_128x128_ws --arch_name systolic_array --SRAM_row_size 2 --DRAM_row_size 2 --config /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/configs/ispass_ae_run_128x128_ws.cfg
+python3 create_action_count.py --saved_folder ../test_runs --run_name scale_example_run_128x128_ws --arch_name systolic_array --SRAM_row_size 2 --DRAM_row_size 2 --config ../configs/ispass_ae_run_128x128_ws.cfg
 
-cp /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs/scale_example_run_128x128_ws/action_count.yaml ./accelergy_input/action_count.yaml
+cp ../test_runs/scale_example_run_128x128_ws/action_count.yaml ./accelergy_input/action_count.yaml
 
-mv /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/test_runs/scale_example_run_128x128_ws  /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/rundir-accelergy/output/scale_sim_output_scale_example_run_128x128_ws
+mv ../test_runs/scale_example_run_128x128_ws  ./output/scale_sim_output_scale_example_run_128x128_ws
 
--- rundir-accelergy/run_accelergy.sh
+++ rundir-accelergy/run_accelergy.sh
@@ -1,6 +1,6 @@
 accelergy accelergy_input/*.yaml accelergy_input/components/*.yaml -o accelergy_output/scale_example_run_128x128_ws -v 1
 
-mv ./accelergy_output/scale_example_run_128x128_ws  /home/zwan63/Research/scalesim_v3_ispass/public_codebase/scale-sim-v2/rundir-accelergy/output/accelergy_output_scale_example_run_128x128_ws
+mv ./accelergy_output/scale_example_run_128x128_ws  ./output/accelergy_output_scale_example_run_128x128_ws
 
 rm -rf ./accelergy_output
 
